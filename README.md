92553644a0e3926bc98acdab36103707f05ca269
Lampros Karachristos <lamproskarachristos@yahoo.com>
2023-07-03 17:48:08 +0300

    Security changes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # VendingMachine Simulator
+
+## Description
+
+Vending machine simulator is a semister java exercise for students to test their abilities. This particular exercise belongs to the students : 
+
+- Lampros Karachristos (Λαμπρος Καραχρηστος)
+- George Kousanas      (Γεωργιος Κουσανας)
+
+## Instructions
+
+The program will first start on the "VendingMachine Master" window where the user gets to initialize the machine based on he's/her's preferences ! In order for this to work the user needs to follow these instruction or the simulator wont continew to the initialization :
+
+1. The "Rows of shelf" and "Shelves" and the dimensions to be greater than 0
+2. The "Rows of shelf" and "Shelves" to be less than 10
+3. The "Number od products" "Experation date" "Product" "Cost" not empty
+4. The "experation date" to be a valid date !  
--- src/main/java/com/karachristos/vending/vendingmachine/VendingMasterController.java
+++ src/main/java/com/karachristos/vending/vendingmachine/VendingMasterController.java
@@ -1,14 +1,11 @@
 package com.karachristos.vending.vendingmachine;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.karachristos.vending.vendingmachine.entities.Machine;
 import com.karachristos.vending.vendingmachine.entities.Merchant;
 import com.karachristos.vending.vendingmachine.entities.Position;
 import com.karachristos.vending.vendingmachine.entities.subentities.Cordinates;
 import com.karachristos.vending.vendingmachine.entities.subentities.Dimensions;
 import com.karachristos.vending.vendingmachine.utils.CustomListCell;
-import com.karachristos.vending.vendingmachine.utils.MachineAdapter;
 import com.karachristos.vending.vendingmachine.utils.PopupUtils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,11 +21,8 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
@@ -37,18 +31,16 @@ public class VendingMasterController implements Initializable {
 
     public Pane initialize_panel, root_panel, product_details_panel;
 
-    public TextField rows_of_shelf, number_of_shelves
-            ,max_x_dimension , max_y_dimension
-            ,max_z_dimension, number_of_products, cost;
-    public Button machine_submit, submit_product, machine_create ;
+    public TextField rows_of_shelf, number_of_shelves, max_x_dimension, max_y_dimension, max_z_dimension, number_of_products, cost;
+    public Button machine_submit, submit_product, machine_create;
 
-    public Label position_product, product_see ;
+    public Label position_product, product_see;
 
-    public ComboBox<Merchant> product_choose ;
+    public ComboBox<Merchant> product_choose;
 
-    public DatePicker experation_date ;
+    public DatePicker experation_date;
 
-    public ListView<Position> position_listview ;
+    public ListView<Position> position_listview;
 
     private ObservableList<Merchant> allMerchants = FXCollections.observableArrayList(new Data().getMerchants());
 
@@ -60,14 +52,14 @@ public class VendingMasterController implements Initializable {
     }
 
     //Data for the machine initialization
-    private int rows, columns ;
-    private double max_x,max_z,max_y ;
+    private int rows, columns;
+    private double max_x, max_z, max_y;
 
     //Data for the product initialization
     private int number_products;
     private LocalDate experationDate;
-    private Merchant product_choosen ;
-    private double cost_of_product ;
+    private Merchant product_choosen;
+    private double cost_of_product;
 
     private ObservableList<Position> positions = FXCollections.observableArrayList();
 
@@ -92,94 +84,108 @@ public class VendingMasterController implements Initializable {
         product_choose.setItems(allMerchants);
 
 
-        machine_submit.setOnAction(event ->{
-            rows =  Integer.parseInt(rows_of_shelf.getText());
-            columns = Integer.parseInt(number_of_shelves.getText()) ;
+        machine_submit.setOnAction(event -> {
+            try {
+                rows = Integer.parseInt(rows_of_shelf.getText());
+                columns = Integer.parseInt(number_of_shelves.getText());
 
-            max_x = Double.parseDouble(max_x_dimension.getText());
-            max_y = Double.parseDouble(max_y_dimension.getText());
-            max_z = Double.parseDouble(max_z_dimension.getText());
+                max_x = Double.parseDouble(max_x_dimension.getText());
+                max_y = Double.parseDouble(max_y_dimension.getText());
+                max_z = Double.parseDouble(max_z_dimension.getText());
 
-            if(rows == 0 || columns == 0 || max_x == 0.0 || max_y == 0.0 || max_z == 0.0 || rows >10 || columns > 10 ) {
-                PopupUtils.showPopup("None of this values can be zero ! Also you cant use more than 10 shelves or rows !", Alert.AlertType.ERROR);
-            }else{
-                System.out.println("[INFO] Values stored successfully");
+                if (rows == 0 || columns == 0 || max_x == 0.0 || max_y == 0.0 || max_z == 0.0 || rows > 10 || columns > 10) {
+                    PopupUtils.showPopup("None of this values can be zero ! Also you cant use more than 10 shelves or rows !", Alert.AlertType.ERROR);
+                } else {
+                    System.out.println("[INFO] Values stored successfully");
 
-                initialize_panel.setDisable(true);
+                    initialize_panel.setDisable(true);
 
-                double gap = 0;
+                    double gap = 0;
 
-                System.out.println("######DEBUG INFO######");
-                for(int x = 1; x<=columns;x++){
-                    for(int i = 1; i<=rows;i++){
+                    System.out.println("######DEBUG INFO######");
+                    for (int x = 1; x <= columns; x++) {
+                        for (int i = 1; i <= rows; i++) {
 
-                        if (i-1 == 0) {
-                            gap = 32.0;
+                            if (i - 1 == 0) {
+                                gap = 32.0;
+                            }
+
+                            double cordinate_x = ((32.0 * (i - 1)) + gap);
+                            double cordinate_y = ((32.0 * (x - 1)) + gap);
+
+                            String position_name = x + "" + i;
+                            System.out.println("[DEBUG]Name : " + position_name + " Cordinates -> x:" + cordinate_x + " y:" + cordinate_y + gap);
+
+
+                            positions.add(new Position(position_name, new Dimensions(max_x, max_y, max_z), new Cordinates(cordinate_x, cordinate_y)));
+
                         }
-
-                        double cordinate_x = ((32.0 * (i-1)) + gap);
-                        double cordinate_y = ((32.0 * (x-1)) + gap);
-
-                        String position_name = x+""+i;
-                        System.out.println("[DEBUG]Name : " + position_name +" Cordinates -> x:" + cordinate_x + " y:" + cordinate_y + gap);
-
-
-                        positions.add(new Position(position_name, new Dimensions(max_x,max_y,max_z), new Cordinates(cordinate_x, cordinate_y)));
-
                     }
+                    System.out.println("[DEBUG]Position count :" + positions.size());
+                    System.out.println("###### END OF DEBUG INFO######");
+
+                    position_listview.setItems(positions);
+                    machine_create.setDisable(false);
                 }
-                System.out.println("[DEBUG]Position count :" + positions.size());
-                System.out.println("###### END OF DEBUG INFO######");
-
-                position_listview.setItems(positions);
-                machine_create.setDisable(false);
+            }catch (Exception e){
+                PopupUtils.showPopup("Something Went wrong check again the values", Alert.AlertType.ERROR);
             }
-
         });
 
-        submit_product.setOnAction(event ->{
-            number_products = Integer.parseInt(number_of_products.getText());
-            experationDate = experation_date.getValue() ;
-            product_choosen = product_choose.getValue() ;
-            cost_of_product = Double.parseDouble(cost.getText());
-            int selectedIndex = position_listview.getSelectionModel().getSelectedIndex();
+        submit_product.setOnAction(event -> {
+            try {
+                number_products = Integer.parseInt(number_of_products.getText());
+                experationDate = experation_date.getValue();
+                product_choosen = product_choose.getValue();
+                cost_of_product = Double.parseDouble(cost.getText());
+                int selectedIndex = position_listview.getSelectionModel().getSelectedIndex();
 
-            ArrayList<Merchant> merchantsToAdd = new ArrayList<>();
+                ArrayList<Merchant> merchantsToAdd = new ArrayList<>();
 
-            Optional<Position> optionalItem = positions.stream()
-                    .filter(item -> item.getPosition_name().equals(position_product.getText().toString()))
-                    .findFirst();
+                Optional<Position> optionalItem = positions.stream()
+                        .filter(item -> item.getPosition_name().equals(position_product.getText().toString()))
+                        .findFirst();
 
-            System.out.println("###### Debug info ######");
-            optionalItem.ifPresent(item -> {
+                System.out.println("###### Debug info ######");
+                optionalItem.ifPresent(item -> {
 
-                if(number_products == 0 || !checkExpiration(experation_date.getValue().atStartOfDay()) ||
-                product_choose == null || cost_of_product == 0 || !item.checkDimensions()){
-                    PopupUtils.showPopup("Failed to Submit Product ! Check the dimesnions or thge experation date or the cost or even you could have forgot to choose one :(", Alert.AlertType.WARNING);
-                }else {
-                    product_choosen.setCost(cost_of_product);
-                    product_choosen.setPosition(item.getPosition_name());
+                    if (number_products == 0 || !checkExpiration(experation_date.getValue().atStartOfDay()) ||
+                            product_choose == null || cost_of_product == 0 || !item.checkDimensions()) {
+                        PopupUtils.showPopup("Failed to Submit Product ! Check the dimesnions or thge experation date or the cost or even you could have forgot to choose one :(", Alert.AlertType.WARNING);
+                    } else {
+                        product_choosen.setCost(cost_of_product);
+                        product_choosen.setPosition(item.getPosition_name());
 
-                    for(int x=0;x<number_products;x++){
-                        merchantsToAdd.add(product_choosen);
+                        for (int x = 0; x < number_products; x++) {
+                            merchantsToAdd.add(product_choosen);
+                        }
+
+                        item.setMerchants(merchantsToAdd);
+                        System.out.println("[DEBUG]Added " + number_products + " Merchants in position " + item.getPosition_name());
+                        product_details_panel.setDisable(true);
                     }
+                });
 
-                    item.setMerchants(merchantsToAdd);
-                    System.out.println("[DEBUG]Added "+number_products + " Merchants in position " + item.getPosition_name());
-                    product_details_panel.setDisable(true);
-                }
-            });
-
-            System.out.println("######End Debug info ######");
-
+                System.out.println("######End Debug info ######");
+            }catch (Exception e){
+                PopupUtils.showPopup("Something Went wrong check again the values", Alert.AlertType.ERROR);
+            }
         });
 
         machine_create.setOnAction(event -> {
-            Machine machine = new Machine(positions,rows ,columns);
+            Machine machine = new Machine(positions, rows, columns);
             machine.setFirst(false);
             machine.setLastUpdated(LocalDateTime.now());
             new Data().writeMachine(machine);
             switchToTargetController(event);
+        });
+
+        product_see.setOnMouseClicked(event -> {
+            if (product_choose.getValue() != null) {
+                PopupUtils.showPopup(product_choose.getValue().toString(), Alert.AlertType.INFORMATION);
+            }else{
+                PopupUtils.showPopup("No product", Alert.AlertType.WARNING);
+            }
         });
 
     }
@@ -205,13 +211,13 @@ public class VendingMasterController implements Initializable {
 
     public static Boolean checkExpiration(LocalDateTime expirationDateTime) {
         LocalDateTime currentDateTime = LocalDateTime.now();
-        Boolean toReturn = true ;
+        Boolean toReturn = true;
 
         if (currentDateTime.isAfter(expirationDateTime)) {
-            toReturn = false ;
+            toReturn = false;
         }
 
-        return toReturn ;
+        return toReturn;
     }
 
 }
